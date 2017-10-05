a466e3574bc8515e18d6aeb3c39a322f8c273cfa
DESKTOP-4SMLB1E\Home <zaicev.vasili@gmail.com>
2017-10-05 22:10:55 +0300

    Add life cycle method

--- app/src/main/java/com/github/vasiliz/myapplication/MainActivity.java
+++ app/src/main/java/com/github/vasiliz/myapplication/MainActivity.java
@@ -1,13 +1,19 @@
 package com.github.vasiliz.myapplication;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable final Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
     }
 }
